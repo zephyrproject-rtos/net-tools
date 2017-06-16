e217dab85cb1617c1900e1263c96b216690b09e5
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2017-06-16 17:51:32 +0300

    stunnel: Add info how to run echo apps over encrypted link
    It is possible to use existing echo-client and echo-server apps
    in Linux and pipe the network traffic inside a SSL tunnel. This
    requires that the Zephyr echo-server and echo-client samples have
    TLS support enabled.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ stunnel.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CONF_FILE=stunnel.conf
+
+if [ ! -f $CONF_FILE ]; then
+    echo "$0: Configuration file $CONF_FILE not found"
+    exit 1
+fi
+
+stunnel $CONF_FILE
