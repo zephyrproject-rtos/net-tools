37d18e7506eec6fa3a99e92a2ac04c6dea07233f
Jukka Rissanen <jukka.rissanen@linux.intel.com>
2019-09-19 14:32:57 +0300

    websocket-server: Add python-websocket-server for testing the client
    The python-websocket-server comes from
    https://github.com/Pithikos/python-websocket-server
    commit fd0b190de3bef32967f64f673897a96544166593
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

---
+++ python-websocket-server/tests/test_handshake.py
@@ -0,0 +1,18 @@
+import _bootstrap_
+from websocket_server import WebSocketHandler
+
+
+def test_hash_calculations_for_response():
+    assert WebSocketHandler.calculate_response_key('zyjFH2rQwrTtNFk5lwEMQg==') == '2hnZADGmT/V1/w1GJYBtttUKASY='
+
+
+def test_response_messages():
+	key = 'zyjFH2rQwrTtNFk5lwEMQg=='
+	expected = \
+		'HTTP/1.1 101 Switching Protocols\r\n'\
+		'Upgrade: websocket\r\n'              \
+		'Connection: Upgrade\r\n'             \
+		'Sec-WebSocket-Accept: 2hnZADGmT/V1/w1GJYBtttUKASY=\r\n'\
+		'\r\n'
+	handshake_content = WebSocketHandler.make_handshake_response(key)
+	assert handshake_content == expected
