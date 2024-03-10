a512e27799f0c710a245ca0224565854c902b149
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-03-10 14:16:28 +0200

    docker: Add connectivity-check.sh script
    This will start dnsmasq and http-server inside Docker so that
    Zephyr connectivity_check sample has a peer to connect to.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

---
+++ docker/connectivity-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright (c) 2024 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+#
+# Start dnsmasq and http server and listen connections. This script is run
+# inside Docker container.
+
+dnsmasq -C /etc/dnsmasq.conf &
+/usr/local/bin/http-server.py
