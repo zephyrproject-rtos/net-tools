4bc293046b90908773035f592a82993b65c91510
Jukka Rissanen <jukka.rissanen@nordicsemi.no>
2024-03-10 15:26:21 +0200

    docker: Convert http-server.py to use python3
    Python2 is no longer in docker image so use python3 instead.
    
    Signed-off-by: Jukka Rissanen <jukka.rissanen@nordicsemi.no>

--- docker/http-server.py
+++ docker/http-server.py
@@ -1,15 +1,14 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import socket
-from BaseHTTPServer import HTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT = 8000
 
 class HTTPServerV6(HTTPServer):
     address_family = socket.AF_INET6
 
-class RequestHandler(SimpleHTTPRequestHandler):
+class RequestHandler(BaseHTTPRequestHandler):
     length = 0
 
     def _set_headers(self):
@@ -22,11 +21,17 @@ class RequestHandler(SimpleHTTPRequestHandler):
         payload = "<html><p>Done</p></html>"
         self.length = len(payload)
         self._set_headers()
-        self.wfile.write(payload)
+        self.wfile.write(payload.encode())
+
+    def do_GET(self):
+        payload = "<html><p>Done</p></html>"
+        self.length = len(payload)
+        self._set_headers()
+        self.wfile.write(payload.encode())
 
 def main():
     httpd = HTTPServerV6(("", PORT), RequestHandler)
-    print "Serving at port", PORT
+    print("Serving at port", PORT)
     httpd.serve_forever()
 
 if __name__ == '__main__':
